37bd65d9f558ebab54c3285511a3794bce99e56f
Samuel vergniol <samuelvgnl@gmail.com>
2022-11-08 09:08:00 +0100

    add header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,28 @@
 import React from "react";
+
 import "./header.css";
 import CTA from "./CTA";
+import HeaderSocials from "./HeaderSocials";
+
+import ME from "../../assets/me.png";
 
 const Header = () => {
   return (
-    <header>
+    <header id="home">
       <div className="container header__container">
-        <h5>Hello I'm</h5>
+        <h5>Bonjour je suis</h5>
         <h1>Samuel VERGNIOL</h1>
-        <h5 className="text-light"> Fullstack Developer</h5>
+        <h5 className="text-light">Développeur Fullstack JavaScript</h5>
         <CTA />
+        <HeaderSocials />
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className="scroll_down">
+          Scroll Down
+        </a>
       </div>
     </header>
   );
