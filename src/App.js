283d3edb2e4a25d4aab12dcf88b683703af36f3d
Samuel vergniol <samuelvgnl@gmail.com>
2022-11-08 09:07:43 +0100

    create main app

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import Nav from "./components/nav/Nav";
 import About from "./components/about/About";
 import Experience from "./components/experience/Experience";
 import Services from "./components/services/Services";
-import Portfolio from "./components/portfolio/Portfolio";
-import Testimonnials from "./components/testimonials/Testimonials";
+// import Portfolio from "./components/portfolio/Portfolio";
+// import Testimonnials from "./components/testimonials/Testimonials";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
 
-import "./App.css";
+import "./index.css";
 
 function App() {
   return (
@@ -18,8 +18,8 @@ function App() {
       <About />
       <Experience />
       <Services />
-      <Portfolio />
-      <Testimonnials />
+      {/* <Portfolio /> */}
+      {/* <Testimonnials /> */}
       <Contact />
       <Footer />
     </>
