30d5329e16a4f1dd0513f5cd3260f96c1b2e0d95
Samuel vergniol <samuelvgnl@gmail.com>
2022-11-08 09:09:38 +0100

    add about and experience

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,54 @@
 import React from "react";
+
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
+
 import "./about.css";
+import me from "../../assets/me-about.jpg";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <section id="about">
+      <h5>A savoir</h5>
+      <h2>A propos de moi</h2>
+
+      <div className="container about_container">
+        <div className="about_me">
+          <div className="about_me-image">
+            <img src={me} alt="" />
+          </div>
+        </div>
+
+        <div className="about_content">
+          <div className="about_cards">
+            <article className="about_card">
+              <FaAward className="about_icon" />
+              <h5>Expérience</h5>
+              <small>1,5 ans d'expérience</small>
+            </article>
+            <article className="about_card">
+              <FiUsers className="about_icon" />
+              <h5>Client</h5>
+              <small>1</small>
+            </article>
+            <article className="about_card">
+              <VscFolderLibrary className="about_icon" />
+              <h5>Projet</h5>
+              <small>8</small>
+            </article>
+          </div>
+          <p>
+            Apres avoir terminé mon Master en alternace mi-septemble je suis a
+            la recheche d'une nouvelle avanture
+          </p>
+          <a href="#contact" className="btn btn-primary">
+            Parlons en
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,8 +1,164 @@
 import React from "react";
 import "./experience.css";
 
+import { BsPatchCheckFill } from "react-icons/bs";
+
 const Experience = () => {
-  return <div>Experience</div>;
+  return (
+    <section id="experience">
+      <h5>Ce que j'ai a proposer</h5>
+      <h2>Mes compétences</h2>
+      <div className="container experience_container">
+        <div>
+          <h3>Développement Frontend</h3>
+          <div className="experience_content">
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details-icon" />
+              <div>
+                <h4>HTML/CSS</h4>
+                <small className="text-light">Expérimenté</small>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details-icon" />
+              <div>
+                <h4>Flutter</h4>
+                <small className="text-light">Basique</small>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details-icon" />
+              <div>
+                <h4>Vue.js</h4>
+                <small className="text-light">Intermédiaire</small>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details-icon" />
+              <div>
+                <h4>React</h4>
+                <small className="text-light">Basique</small>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details-icon" />
+              <div>
+                <h4>JavaScript</h4>
+                <small className="text-light">Intermédiaire</small>
+              </div>
+            </article>
+          </div>
+        </div>
+
+        <div>
+          <h3>Développement Backend</h3>
+          <div className="experience_content">
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details-icon" />
+              <div>
+                <h4>Node.js</h4>
+                <small className="text-light">Intermédiaire</small>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details-icon" />
+              <div>
+                <h4>Express</h4>
+                <small className="text-light">Intermédiaire</small>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details-icon" />
+              <div>
+                <h4>MongoDB</h4>
+                <small className="text-light">Intermédiaire</small>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details-icon" />
+              <div>
+                <h4>PostgreSQL</h4>
+                <small className="text-light">Intermédiaire</small>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details-icon" />
+              <div>
+                <h4>Python</h4>
+                <small className="text-light">Intermédiaire</small>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details-icon" />
+              <div>
+                <h4>Elixir</h4>
+                <small className="text-light">Basique</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        <div>
+          <h3>Blockchaine</h3>
+          <div className="experience_content">
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details-icon" />
+              <div>
+                <h4>Solidity</h4>
+                <small className="text-light">Basique</small>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details-icon" />
+              <div>
+                <h4>Truffle suite</h4>
+                <small className="text-light">Basique</small>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details-icon" />
+              <div>
+                <h4>Hardhat</h4>
+                <small className="text-light">Basique</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        <div>
+          <h3>Déploiement</h3>
+          <div className="experience_content">
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details-icon" />
+              <div>
+                <h4>Docker</h4>
+                <small className="text-light">Intermédiaire</small>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details-icon" />
+              <div>
+                <h4>Kubernetes</h4>
+                <small className="text-light">Basique</small>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details-icon" />
+              <div>
+                <h4>Ansible</h4>
+                <small className="text-light">Basique</small>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details-icon" />
+              <div>
+                <h4>Jenkins</h4>
+                <small className="text-light">Basique</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Experience;
